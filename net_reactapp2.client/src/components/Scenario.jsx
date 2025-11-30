f3ba7fb4b7ed51f9a1bcb6fe2bc6859f376dc029
Aina <Aina@LAPTOP-721RF3FI>
2025-11-30 09:59:12 +0100

    UI modification to Scenario

--- net_reactapp2.client/src/components/Scenario.jsx
+++ net_reactapp2.client/src/components/Scenario.jsx
@@ -8,15 +8,21 @@ function Scenario({ scenario, navigator }) {
     }
 
   return (
-      <div className="bg-light col-md-4 bg-light border rounded-pill d-flex align-items-center">
-          <div className="col-md-10 m-1 row justify-content-center">
-              <h5 className="col-sm-12">{scenario.name}</h5>
-              <h6 className="col-sm-6">Pots: {scenario.numberOfPot}</h6>
-              <h6 className="col-sm-6">Teams per pot: {scenario.numberOfTeamsPerPot}</h6>
-              <h6 className="col-sm-6">Games per pot: {scenario.numberOfGamesPerPot}</h6>
-              <h6 className="col-sm-6">Home and Away: {scenario.homeAndAwayPerOpponent? "Yes": "No"}</h6>
-          </div>          
-          <Pencil className="btn col-md-2 fs-4 m-0" onClick={editScenario} size={55} color="blue" />
+      <div className="row bg-light col-md-4 border rounded-pill d-flex p-0 m-0">
+          <div className="align-items-center col-md-12 row p-0 m-0">
+              <div className="col-md-10 m-0 row justify-content-center">
+                  <h4 className="col-sm-12 text-center">{scenario.name}</h4>
+                  <h6 className="col-sm-6">Pots: {scenario.numberOfPot}</h6>
+                  <h6 className="col-sm-6">Teams per pot: {scenario.numberOfTeamsPerPot}</h6>
+                  <h6 className="col-sm-6">Games per pot: {scenario.numberOfGamesPerPot}</h6>
+                  <h6 className="col-sm-6">Home & Away: {scenario.homeAndAwayPerOpponent ? "Yes" : "No"}</h6>
+              </div>
+              <Pencil className="btn col-md-2 fs-4 m-0" onClick={editScenario} size={55} color="blue" />
+          </div>  
+          <div className="col-md-12 row" >
+              <div className="col-md-3"/>
+              <h5 className="col-md-5 btn btn-primary border text-center">Run simulation</h5>
+          </div>
       </div>     
   );
 }
